cd3bac9b895e174d6469dd45ccf5da3e401d8350
katumas <katumwota@gmail.com>
2017-12-13 21:44:21 +0200

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="jumbotron">
         <h1 class="display-3">Hello, world!</h1>
         <p class="lead">We #love #coding.</p>
+        <p>Chech projects below!</p>
         <p class="lead">
             <a class="btn btn-secondary btn-lg" href="#" role="button">Watch our projects!</a>
         </p>
