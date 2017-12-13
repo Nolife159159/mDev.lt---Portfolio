8129a83769d4d1f3617e3e9a5e8d34d8b8efd468
katumas <katumwota@gmail.com>
2017-12-13 21:46:07 +0200

    Simple check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron">
         <h1 class="display-3">Hello, world!</h1>
         <p class="lead">We #love #coding.</p>
-        <p>Chech projects below!</p>
+        <p>Chech projects below!Now!</p>
         <p class="lead">
             <a class="btn btn-secondary btn-lg" href="#" role="button">Watch our projects!</a>
         </p>
