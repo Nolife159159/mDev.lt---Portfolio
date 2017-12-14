c3e7f9e5f8a458d79efac0a4f53eb249e79892c5
Nolife159159 <mants.mockevicius@gmail.com>
2017-12-14 18:13:43 +0200

    Title for 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+<head>
+    <title>{{ config('app.name') }} - 404 Not Found</title>
+</head>
+
 @section('content')
     <center>
         <h1 style="margin:0; font-size:150px; line-height:150px; font-weight:bold;">404</h1>
