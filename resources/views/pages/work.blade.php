ed8952c4f8600d5cf58cf19b228d63f5b4770946
katumas <katumwota@gmail.com>
2017-12-14 21:21:33 +0200

    Some changes.

---
+++ resources/views/pages/work.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-0 row bg-gradient-dark text-white">
+        <div class="col-sm border border-secondary">
+            One of three columns
+        </div>
+        <div class="col-sm border border-secondary">
+            One of three columns
+        </div>
+        <div class="col-sm border border-secondary">
+            One of three columns
+        </div>
+    </div>
+@endsection
