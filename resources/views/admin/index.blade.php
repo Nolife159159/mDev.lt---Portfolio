7985286adb6c4ea68e913468c79864e18facfa62
Nolife159159 <mants.mockevicius@gmail.com>
2017-12-17 21:15:52 +0200

    Major CSS fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,16 +4,16 @@
 
 @section('content')
     <!--Admin page sidebar meniu -->
-    <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+    <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-dark sidebar">
         <ul class="nav nav-pills flex-column">
             <li class="nav-item">
-                <a class="nav-link btn-danger" href="#">Overview <span class="sr-only">(current)</span></a>
+                <a class="nav-link btn-danger text-white"  href="{{ url('/admin') }}"><i class="fa fa-tachometer fa-1x text-white" aria-hidden="true"></i> Overview</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ url('/admin/upload') }}">Upload new work</a>
+                <a class="nav-link text-white" href="{{ url('/admin/upload') }}"><i class="fa fa-upload fa-1x text-danger" aria-hidden="true"></i> Upload new work</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Edit a work</a>
+            </l class="nav-item">
+                <a class="nav-link  text-white" href="#"><i class="fa fa-pencil fa-1x text-danger" aria-hidden="true"></i> Edit a work</a>
             </li>
         </ul>
     </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,16 @@
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active m-2">
-                    <a class="nav-link" href="{{ url('/') }}"><i class="fa fa-home fa-2x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Homepage</span></a>
+                    <a class="nav-link" href="{{ url('/') }}"><i class="fa fa-home fa-1x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Homepage</span></a>
                 </li>
                 <li class="nav-item m-2">
-                    <a class="nav-link" href="{{ url('/about-us') }}"><i class="fa fa-user fa-2x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">About US</span></a>
+                    <a class="nav-link" href="{{ url('/about-us') }}"><i class="fa fa-user fa-1x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">About US</span></a>
                 </li>
                 <li class="nav-item m-2">
-                    <a class="nav-link" href="{{ url('/our-work') }}"><i class="fa fa-eye fa-2x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Our Work</span></a>
+                    <a class="nav-link" href="{{ url('/our-work') }}"><i class="fa fa-eye fa-1x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Our Work</span></a>
                 </li>
                 <li class="nav-item m-2">
-                    <a class="nav-link" href="{{ url('/contact-us') }}"><i class="fa fa-envelope fa-2x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Contact Us</span></a>
+                    <a class="nav-link" href="{{ url('/contact-us') }}"><i class="fa fa-envelope fa-1x text-danger" aria-hidden="true"></i> <span class="text-uppercase border border-danger border-right-0 border-top-0 border-left-0">Contact Us</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav">
